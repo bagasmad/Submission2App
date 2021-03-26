e255279e8dcee41a6c137900b9c5c01e9500a6cc
bagasmad <bagas.m@mail.ugm.ac.id>
2021-03-27 01:21:25 +0700

    New build

--- app/src/main/java/com/example/submission1app/CardViewAdapter.kt
+++ app/src/main/java/com/example/submission1app/CardViewAdapter.kt
@@ -1,23 +1,26 @@
 package com.example.submission1app
 
 import android.content.Context
+import android.media.MediaRouter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.submission1app.databinding.CardGithubRecyclerBinding
 
-class CardViewAdapter(private val listUserDatas: ArrayList<UserDatas>) :
-    RecyclerView.Adapter<CardViewAdapter.CardViewViewHolder>() {
-    private lateinit var context: Context
+class CardViewAdapter(private val context: Context) : RecyclerView.Adapter<CardViewAdapter.CardViewViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
+    private val mData = ArrayList<UserDatas>()
+
+    fun setData(items: ArrayList<UserDatas>) {
+        mData.clear()
+        mData.addAll(items)
+        notifyDataSetChanged()
+    }
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
-    }
 
-    fun setContext(context: Context) {
-        this.context = context
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CardViewViewHolder {
@@ -27,7 +30,7 @@ class CardViewAdapter(private val listUserDatas: ArrayList<UserDatas>) :
     }
 
     override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
-        val user = listUserDatas[position]
+        val user = mData[position]
         holder.binding.imageDisplayPicture.setImageResource(
             context.resources.getIdentifier(
                 user.avatar?.substring(
@@ -46,7 +49,7 @@ class CardViewAdapter(private val listUserDatas: ArrayList<UserDatas>) :
     }
 
     override fun getItemCount(): Int {
-        return listUserDatas.size
+        return mData.size
     }
 
     class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/example/submission1app/MainActivity.kt
+++ app/src/main/java/com/example/submission1app/MainActivity.kt
@@ -4,61 +4,51 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.submission1app.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private var list: ArrayList<UserDatas> = arrayListOf()
+    private lateinit var adapter: CardViewAdapter
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        fun getJsonFromFile(context: Context, fileName: String): String? {
-            val stringFromJson: String
-            try {
-                //Mencoba untuk parsing data Json menjadi string
-                stringFromJson = context.assets.open(fileName).bufferedReader().use {
-                    it.readText()
-                }
-            } catch (ioException: IOException) {
-                ioException.printStackTrace()
-                return null
-            }
-            return stringFromJson
-        }
-
-        val jsonFileString = getJsonFromFile(applicationContext, "githubuser.json")
-        val gson = Gson()
-        val usersRoot: User =
-            gson.fromJson(jsonFileString, User::class.java) //parsing JSON menggunakan GSON
-        usersRoot.users.forEachIndexed { _, users -> list.add(users) }
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+        mainViewModel.setUserDatas(this@MainActivity)
+        adapter = CardViewAdapter(this)
         showRecyclerView()
         binding.buttonAbout.setOnClickListener {
             val aboutIntent = Intent(this@MainActivity, AboutActivity::class.java)
             startActivity(aboutIntent)
         }
 
+        mainViewModel.getListUserDatas().observe(this,{userDatas ->
+            if (userDatas!=null)
+            {
+                adapter.setData(userDatas)
+                adapter.setOnItemClickCallback(object : CardViewAdapter.OnItemClickCallback {
+                    override fun onItemClicked(data: UserDatas) {
+                        val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
+                        detailIntent.putExtra(DetailActivity.EXTRA_USER_DATA, data)
+                        startActivity(detailIntent)
+                    }
+
+                })
+            }
+        })
+
 
     }
 
     private fun showRecyclerView() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        val listUserAdapter = CardViewAdapter(list)
-        binding.recyclerView.adapter = listUserAdapter
-        listUserAdapter.setOnItemClickCallback(object : CardViewAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: UserDatas) {
-                val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
-                detailIntent.putExtra(DetailActivity.EXTRA_USER_DATA, data)
-                startActivity(detailIntent)
-            }
-
-        })
-        listUserAdapter.setContext(this)
+        binding.recyclerView.adapter = adapter
 
     }
 }
